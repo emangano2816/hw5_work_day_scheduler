da50e4c9266125700777a0b8278cf47253e9b4a7
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-05-19 15:00:30 -0400

    Modified code. index.html: added another row in the calendar to test js. script.js: added code to color the event portion of the calendar based on the time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
-    var dateDisplay = $("#currentDay");
-    dateDisplay.text(moment().format("dddd, MMMM Do"));
+    //Setting date to current date
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+    var hourEl = $('.hour');
+    var eventEl = $('.textarea');
+    
+    hourEl.each(function (i) {
+        var currentHour = moment().format('hA');
+        var rowHour = moment(this.innerText,'hA');
+
+        console.log(currentHour);
+        console.log(rowHour);
+        
+
+        if (moment(currentHour,'hA').isAfter(rowHour,'hA')){
+            console.log('after');
+            eventEl[i].classList.add('past');
+        } else if (moment(currentHour,'hA').isSame(rowHour,'hA')) {
+            console.log('same');
+            eventEl[i].classList.add('present');
+        } else {
+            console.log('before');
+            eventEl[i].classList.add('future');
+        }      
+
+    })
 
 
 
